4bd9d4a8110832eb65bf365cfe997f4e7dd9aa5c
iamprashant <mrprashantit@gmail.com>
2023-08-23 21:43:10 +0530

    first commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledRecord = require('./build/Record.json');
+
+//Link to rinkeby network by using Infura and providing seed phrase of metamask wallet
+const provider = new HDWalletProvider(
+    'order federal primary pave bean yard clerk ocean wear bless choose pencil',
+    'http://localhost:7545/',
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    //Deploy contract to rinkeby network
+    const result = await new web3.eth.Contract(JSON.parse(compiledRecord.interface))
+        .deploy({ data: compiledRecord.bytecode })
+        .send({ gas: '4612388', from: accounts[0] });
+
+    //Display the address of the contract 
+    console.log('Contract deployed to', result.options.address);
+
+    //Always go to record.js after updating solidity code
+};
+
+deploy();
